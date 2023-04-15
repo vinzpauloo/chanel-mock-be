16dece755230d65bed5d2908dd63ec179f4c15a8
Vincent Paul Lim Oo <oo.vincentpaul@1bit.com.ph>
2023-04-15 13:17:28 +0800

    Initial commit

---
+++ src/main/java/com/margaux/margauxloginapi/MargauxLoginApiApplication.java
@@ -0,0 +1,13 @@
+package com.margaux.margauxloginapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MargauxLoginApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MargauxLoginApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/margaux/margauxloginapi/MargauxLoginApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.margaux.margauxloginapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MargauxLoginApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
